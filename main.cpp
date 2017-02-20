fcc9d63d71b9fced64c8b2d9a527766bf97466c7
Nitish Garg <nitish.garg.6174@gmail.com>
2017-02-20 11:58:51 +0530

    Update repository

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ int main(int argc, char **argv)
     int no_of_buffer, key_per_page, buffer_per_input,buffer_available,keys;
     char filename[100];
     string s;
-    int n,num;
-    std::vector<int> a,b,c;
+    int n, num, i;
+    std::vector<int> a, b, c;
+    int pass, run, last;
     fstream f;
 
     if(argc!=5)
@@ -21,68 +22,70 @@ int main(int argc, char **argv)
         buffer_per_input = atoi(argv[3]);
         strcpy(filename, argv[4]);
 
-        f.open(filename,ios::in);
-        while(getline(f,s))
-            a.push_back(atoi(s.c_str()));
-        n = a.size();
-        cout<<n<<endl;
-
-        cout<<"Input :"<<endl;
-        display(a,0,n,0);
-        cout<<"----------"<<endl<<endl;
-
-        buffer_available = no_of_buffer/buffer_per_input;
-        keys = no_of_buffer * key_per_page;
-
-        cout<<"Pass 0:"<<endl;
-        int run=1;
-        int last;
-        int count=0;
-        for(int i=0;i<n;i+=keys)
+        if(3*buffer_per_input>no_of_buffer)
+            cout<<"This set of parameters is invalid"<<endl;
+        else
         {
-            b.push_back(i);
-            cout<<"  Run "<<run<<":"<<endl;
-            if(i+keys>=n)
-                last = n;
-            else
-                last = i+keys;
-            cout<<"    ";
-            display(a,i,last,0);
-            sort(a.begin() + i, a.begin() + last);
-            cout<<"    Output:"<<endl;
-            cout<<"    ";
-            display(a,i,last,0);
-            run++;
-        }
-        cout<<"Total Input Runs in Pass 0: "<<n<<endl;
-        cout<<"Total Output Runs Produced after Pass 0: "<<run-1 <<endl;
-        int pass=1;
-        while(b.size()>1)
-        {
-            cout<<endl;
+            f.open(filename,ios::in);
+            while(getline(f,s))
+                a.push_back(atoi(s.c_str()));
+            n = a.size();
+
+            cout<<"Input :"<<endl;
+            display(a,0,n,0);
+            cout<<"----------"<<endl<<endl;
+
+            buffer_available = no_of_buffer/buffer_per_input;
+            keys = no_of_buffer * key_per_page;
+
+            pass = 0;
+            run = 1;
             cout<<"Pass "<<pass<<":"<<endl;
-            pass++;
-            run=1;
-            for(int i=0;i<b.size();i+=(buffer_available-1))
+            for( i=0 ; i<n ; i+=keys )
             {
-                c.push_back(b[i]);
-                if((i+buffer_available-1)>=b.size())
+                b.push_back(i);
+                cout<<"  Run "<<run<<":"<<endl;
+                last = i + keys;
+                if( last > n )
                     last = n;
-                else
-                    last = b[i+buffer_available-1];
-                cout<<"  Run"<<run<<":"<<endl;
                 cout<<"    ";
-                display(a,b[i],last,0);
-                sort(a.begin()+b[i], a.begin()+last);
+                display(a,i,last,0);
+                sort(a.begin() + i, a.begin() + last);
                 cout<<"    Output:"<<endl;
                 cout<<"    ";
-                display(a,b[i],last,0);
+                display(a,i,last,0);
                 run++;
             }
-            cout<<"Total Input Runs in Pass "<<pass-1<<": "<<b.size()<<endl;
-            cout<<"Total Output Runs Produced after Pass 0: "<<run-1<<endl ;
-            b=c;
-            c.clear();
+            cout<<"Total Input Runs in Pass "<<pass<<": "<<n<<endl;
+            cout<<"Total Output Runs Produced after Pass "<<pass<<": "<<run-1<<endl;
+            pass++;
+            while(b.size()>1)
+            {
+                cout<<endl;
+                cout<<"Pass "<<pass<<":"<<endl;
+                run = 1;
+                for( i=0 ; i<b.size() ; i+=(buffer_available-1) )
+                {
+                    c.push_back(b[i]);
+                    if((i+buffer_available-1)>=b.size())
+                        last = n;
+                    else
+                        last = b[i+buffer_available-1];
+                    cout<<"  Run"<<run<<":"<<endl;
+                    cout<<"    ";
+                    display(a,b[i],last,0);
+                    sort(a.begin()+b[i], a.begin()+last);
+                    cout<<"    Output:"<<endl;
+                    cout<<"    ";
+                    display(a,b[i],last,0);
+                    run++;
+                }
+                cout<<"Total Input Runs in Pass "<<pass<<": "<<b.size()<<endl;
+                cout<<"Total Output Runs Produced after Pass "<<pass<<": "<<run-1<<endl ;
+                b=c;
+                c.clear();
+                pass++;
+            }
         }
     }
 
